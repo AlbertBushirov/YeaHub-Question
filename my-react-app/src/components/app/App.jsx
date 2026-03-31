c10a05c70928315c8d2bd1b735a19afc6ad546c2
Albert Bashirov <albash7@yandex.ru>
2026-03-31 12:48:44 +0300

    преднастройка и разметка

---
+++ my-react-app/src/components/app/App.jsx
@@ -0,0 +1,8 @@
+import { Header } from "../header/header";
+import "./App.css";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ my-react-app/src/components/header/header.jsx
@@ -0,0 +1,24 @@
+import "./header.scss";
+
+export function Header() {
+  return (
+    <header className="header">
+      <a href="#" className="header__logo">
+        {" "}
+        <img src="" alt="" />
+      </a>
+      <nav className="header__nav">
+        <ul>
+          <li>База вопросов</li>
+          <li> Тренажёр</li>
+          <li>Материалы</li>
+          <li>Навыки()</li>
+        </ul>
+      </nav>
+      <div className="header__buttons">
+        <button>Вход</button>
+        <button>Регистрация</button>
+      </div>
+    </header>
+  );
+}
